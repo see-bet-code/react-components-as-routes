1184c45d6571c61b412fa8cff409f4152cda69e6
see-bet-code <brittani.elan.taylor@gmail.com>
2020-11-28 20:50:31 -0500

    Done.

---
+++ src/Signup.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class Signup extends React.Component {
+  render() {
+    return (
+      <form>
+        <h1>Signup</h1>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+          <label htmlFor="username">Username</label>
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+          <label htmlFor="password">Password</label>
+        </div>
+        <div>
+          <input type="password" name="passwordConfirmation" placeholder="Confirm Password" />
+          <label htmlFor="passwordConfirmation">Confirm Password</label>
+        </div>
+        <input type="submit" value="Signup" />
+      </form>
+    );
+  }
+}
+
+export default Signup;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Home from './Home'
+import About from './About'
+import Login from './Login'
+import Signup from './Signup'
+import Navbar from './Navbar'
+// Step 1. Import react-router functions
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 
-const Home = () => {
-  return (
+// Step 2. Changed to have router coordinate what is displayed
+ReactDOM.render((
+  <Router>
     <div>
-      <h1>Home!</h1>
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      {/* <Route exact path="/" render={() => <h1>Home!</h1>} /> */}
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/signup" component={Signup} />
     </div>
-  );
-};
-
-ReactDOM.render(
-  <Home />,
+    
+  </Router>),
   document.getElementById('root')
 );
